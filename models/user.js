1bc32787f61e8fe4416c1900778842ce37dfd26a
Tamizh-1153 <128562854+Tamizh-1153@users.noreply.github.com>
2023-10-22 15:30:12 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -20,7 +20,6 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, "Please provide a password"],
-    minlength: 6,
   },
   token: {
     type: String,
